1350c39beedb9fcda194623864f9ca50b3f4d460
Ubuntu <abigeard@abigeard-azure-vm.jo3cbenmacrejgynkvxb3sfh0d.cx.internal.cloudapp.net>
2022-12-06 01:08:01 +0000

    load and save models

--- src/main.py
+++ src/main.py
@@ -26,7 +26,9 @@ parser.add_argument("--val_percentages", default="95,5")
 parser.add_argument("--train_dataset", default="amazon_electronics,amazon_video")
 parser.add_argument("--val_dataset", default="amazon_electronics,amazon_video")
 parser.add_argument("--mode", default="pimped_bert")
-parser.add_argument("--path_ckpt", default="")
+parser.add_argument("--base_model_ckpt", default=None)
+parser.add_argument("--save_path_ckpt", default=None)
+parser.add_argument("--load_path_ckpt", default=None)
 parser.add_argument("--debug", action="store_true")
 parser.add_argument("--repeats", default=1, type=int)
 parser.add_argument("--batch_size", default=8, type=int)
@@ -35,6 +37,7 @@ parser.add_argument("--eval_only", default=0, type=int)
 parser.add_argument("--n_train", default=10000, type=int)
 parser.add_argument("--n_val", default=100, type=int)
 parser.add_argument("--n_epochs", default=5, type=int)
+parser.add_argument("--lr", default=1e-3, type=float)
 args = parser.parse_args()
 
 
@@ -110,27 +113,22 @@ def eval(model, tok, eval_dataloader, mode):
 
 def ft_bert(
     model,
+    optimizer,
     tok,
     train_dataloader,
     eval_dataloader,
     mode,
     saving_path="",
     n_epochs=5,
+    train_dataset=None,
+    train_percentages=None,
+    description_str="",
 ):
     model = copy.deepcopy(model).to(DEVICE)
-
-    if mode != "pimped_bert":
-        optimizer = torch.optim.Adam(parameters_to_fine_tune(model, mode), lr=1e-4)
-    else:
-        model = SurgicalFineTuningBert(model).to(DEVICE)
-        all_params = [p for p in model.parameters() if p.requires_grad]
-        optimizer = torch.optim.Adam(all_params, lr=1e-3)
-        # optimizer = torch.optim.Adam(all_params, lr=1e-2)
 
     print(f"Train samples: {len(train_dataloader)}")
     print(f"Val samples: {len(eval_dataloader)}")
 
-    
     for epoch in range(n_epochs):
         pbar = tqdm.tqdm(enumerate(train_dataloader))
         print(f"Epoch {epoch}")
@@ -161,24 +159,24 @@ def ft_bert(
             if step % 20 == 0:
                 val_acc = eval(model, tok, eval_dataloader, mode)
                 # pbar.set_description(f"Fine-tuning val accuracy: {val_acc:.04f}")
-                print(f"Fine-tuning val accuracy: {val_acc:.04f}") 
+                print(f"Fine-tuning val accuracy: {val_acc:.04f}")
 
                 if mode == "pimped_bert":
                     alphas = model.get_alphas()
                     print(alphas)
                     alphas_opti = torch.ones(len(alphas))
                     alphas_frozen = torch.zeros(len(alphas))
-                    val_acc_opti=eval(
+                    val_acc_opti = eval(
                         lambda x: model.forward_alphas(x, alphas=alphas_opti),
                         tok,
                         eval_dataloader,
-                        mode
+                        mode,
                     )
-                    val_acc_frozen=eval(
+                    val_acc_frozen = eval(
                         lambda x: model.forward_alphas(x, alphas=alphas_frozen),
                         tok,
                         eval_dataloader,
-                        mode
+                        mode,
                     )
                     # pbar.set_description(f"Accuracy opti only: {val_acc_opti:.04f}")
                     # pbar.set_description(f"Accuracy frozen only: {val_acc_frozen:.04f}")
@@ -187,32 +185,50 @@ def ft_bert(
                     print("Alphas: ", alphas)
 
                     f = open(
-                    "src/results/ft/"
-                    + args.model + "-"
-                    + args.mode + "-"
-                    + args.train_dataset + "-"
-                    + args.train_percentages 
-                    + ".txt",
-                    "a")
+                        f"src/results/ft/{description_str}.txt",
+                        "a",
+                    )
 
-                    f.write("Epoch " + str(epoch) + ", Step " + str(step) + " | alphas: " + str(alphas) + " , accuracy: " + str(round(val_acc,4)) + "\n")
+                    f.write(
+                        "Epoch "
+                        + str(epoch)
+                        + ", Step "
+                        + str(step)
+                        + " | alphas: "
+                        + str(alphas)
+                        + " , accuracy: "
+                        + str(round(val_acc, 4))
+                        + "\n"
+                    )
                     f.close()
 
             if step % 50 == 0 and saving_path != "":
 
                 torch.save(
                     {"model_state_dict": model.state_dict()},
-                    saving_path + ".pt",
+                    os.path.join(saving_path, description_str + ".pt"),
                 )
                 f = open(
                     "src/results/ft/"
-                    + args.model + "-"
-                    + args.mode + "-"
-                    + args.train_dataset + "-"
-                    + args.train_percentages 
+                    + model
+                    + "-"
+                    + mode
+                    + "-"
+                    + train_dataset
+                    + "-"
+                    + train_percentages
                     + ".txt",
-                    "a")
-                f.write("Epoch " + str(epoch) + ", Step " + str(step) +  " | Accuracy: " + str(round(val_acc,4)) + "\n")
+                    "a",
+                )
+                f.write(
+                    "Epoch "
+                    + str(epoch)
+                    + ", Step "
+                    + str(step)
+                    + " | Accuracy: "
+                    + str(round(val_acc, 4))
+                    + "\n"
+                )
                 f.close()
 
     return model
@@ -229,6 +245,11 @@ def run_ft(
     n_epochs,
     n_train: int = 1000,
     n_val: int = 100,
+    base_model_ckpt=None,
+    load_path_ckpt=None,
+    save_path_ckpt=None,
+    eval_only=0,
+    learning_rate=1e-3,
 ):
     results = {}
 
@@ -247,7 +268,6 @@ def run_ft(
         if mode not in ["first", "middle", "last", "all", "pimped_bert"]:
             raise ValueError(mode, "is not a valid argument for argument mode")
 
-        model = None
         tokenizer = None
         if "amazon" in train_datasets[0]:
             model, tokenizer = get_model_and_tokenizer(
@@ -262,8 +282,8 @@ def run_ft(
                 num_labels=2,
             )
 
-        eval_only_str = (args.eval_only == 0) * "finetune_and_eval" + (
-            args.eval_only == 1
+        eval_only_str = (eval_only == 0) * "finetune_and_eval" + (
+            eval_only == 1
         ) * "eval_only"
 
         description_str = "_".join(
@@ -284,82 +304,106 @@ def run_ft(
 
         print(f"Begin training for {description_str}")
 
-        for repeat in range(args.repeats):
+        # path_ckpt = f"results/ft/fine_tuned_{description_str}.pt"
 
-            # path_ckpt = f"results/ft/fine_tuned_{description_str}.pt"
-
-            print(f"Beginning repeat #{repeat}")
-            if args.path_ckpt is not None and os.path.exists(args.path_ckpt):
-                ckpt = torch.load(args.path_ckpt)
+        if mode == "pimped_bert":
+            if base_model_ckpt is not None:
+                ckpt = torch.load(base_model_ckpt)
                 model.load_state_dict(ckpt["model_state_dict"])
-
-            if repeat > 0:
-                model = fine_tuned
-
-            train_dataloader = DataLoader(
-                list(zip(train_data["x"], train_data["y"])), batch_size=batch_size
+            model = SurgicalFineTuningBert(bert_model=model).to(DEVICE)
+            if load_path_ckpt is not None:
+                ckpt = torch.load(load_path_ckpt)
+                model.load_state_dict(ckpt["model_state_dict"])
+            all_params = [p for p in model.parameters() if p.requires_grad]
+            optimizer = torch.optim.Adam(all_params, lr=learning_rate)
+        else:
+            if load_path_ckpt is not None:
+                ckpt = torch.load(load_path_ckpt)
+                model.load_state_dict(ckpt["model_state_dict"])
+            optimizer = torch.optim.Adam(
+                parameters_to_fine_tune(model, mode), lr=learning_rate
             )
-            eval_dataloader = DataLoader(
-                list(zip(val_data["x"], val_data["y"])), batch_size=batch_size
+
+        train_dataloader = DataLoader(
+            list(zip(train_data["x"], train_data["y"])), batch_size=batch_size
+        )
+        eval_dataloader = DataLoader(
+            list(zip(val_data["x"], val_data["y"])), batch_size=batch_size
+        )
+
+        if eval_only == 0:
+            fine_tuned = ft_bert(
+                model,
+                optimizer,
+                tokenizer,
+                train_dataloader,
+                eval_dataloader,
+                mode,
+                save_path_ckpt,
+                n_epochs,
+                train_dataset=train_datasets,
+                train_percentages=train_percentages,
+                description_str=description_str,
+            )
+            val_acc = eval(fine_tuned, tokenizer, eval_dataloader, mode)
+        else:
+            val_acc = eval(model, tokenizer, eval_dataloader, mode)
+
+        results[description_str] = val_acc
+
+        question = "ft"
+        if not os.path.exists(f"results/{question}"):
+            os.makedirs(f"results/{question}")
+
+        if eval_only == 0:
+            torch.save(
+                {"model_state_dict": fine_tuned.state_dict()},
+                os.path.join(save_path_ckpt, description_str + ".pt"),
             )
 
-            if args.eval_only == 0:
-                fine_tuned = ft_bert(
-                    model,
-                    tokenizer,
-                    train_dataloader,
-                    eval_dataloader,
-                    mode,
-                    args.path_ckpt[:-3],
-                    n_epochs
-                )
-                val_acc = eval(fine_tuned, tokenizer, eval_dataloader, mode)
-            else:
-                val_acc = eval(model, tokenizer, eval_dataloader, mode)
+        print(results)
 
-            results[description_str] = val_acc
-
-            question = "ft"
-            if not os.path.exists(f"results/{question}"):
-                os.makedirs(f"results/{question}")
-
-            if args.eval_only == 0:
-                torch.save(
-                    {"model_state_dict": fine_tuned.state_dict()},
-                    args.path_ckpt,
-                )
-
-            print(results)
-
-            for k_, v in results.items():
-                with open(f"results/{question}/{k_}.json", "w") as f:
-                    json.dump({"metric": v}, f)
-            results = {}
+        for k_, v in results.items():
+            with open(f"results/{question}/{k_}.json", "w") as f:
+                json.dump({"metric": v}, f)
+        results = {}
 
 
 if __name__ == "__main__":
     train_percentages = [int(k) for k in args.train_percentages.split(",")]
     val_percentages = [int(k) for k in args.val_percentages.split(",")]
-    # run_ft(
-    #     ["bert-tiny"],
-    #     ["amazon_electronics", "amazon_video"],
-    #     ["amazon_electronics", "amazon_video"],
-    #     [95, 5],
-    #     [95, 5],
-    #     ["pimped_bert"],
-    #     args.batch_size,
-    #     args.n_train,
-    #     args.n_val,
-    # )
+
     run_ft(
-        args.model.split(","),
-        args.train_dataset.split(","),
-        args.val_dataset.split(","),
-        train_percentages,
-        val_percentages,
-        args.mode.split(","),
-        args.batch_size,
-        args.n_epochs,
-        args.n_train,
-        args.n_val,
+        models=["bert-med"],
+        train_datasets=["amazon_electronics"],
+        val_datasets=["amazon_electronics"],
+        train_percentages=[100],
+        val_percentages=[100],
+        modes=["pimped_bert"],
+        batch_size=4,
+        n_epochs=1,
+        n_train=10,
+        n_val=1,
+        base_model_ckpt="ckpts/bert-med_train_amazon_electronics_val_amazon_electronics_train_pct_100_val_pct_100_all_finetune_and_eval.pt",
+        load_path_ckpt="ckpts/bert-med_train_amazon_electronics_val_amazon_electronics_train_pct_100_val_pct_100_pimped_bert_finetune_and_eval.pt",
+        save_path_ckpt="ckpts",
+        eval_only=0,
+        learning_rate=1e-3,
     )
+    # run_ft(
+    #     models=args.model.split(","),
+    #     train_datasets=args.train_dataset.split(","),
+    #     val_datasets=args.val_dataset.split(","),
+    #     train_percentages=train_percentages,
+    #     val_percentages=val_percentages,
+    #     modes=args.mode.split(","),
+    #     batch_size=args.batch_size,
+    #     n_epochs=args.n_epochs,
+    #     n_train=args.n_train,
+    #     n_val=args.n_val,
+    #     base_model_ckpt=args.base_model_ckpt,
+    #     load_path_ckpt=args.load_path_ckpt,
+    #     save_path_ckpt=args.save_path_ckpt,
+    #     eval_only=args.eval_only,
+    #     learning_rate=args.lr,
+    # )
